8e374df50d028e4f02c8c4bd15bc61c5e65b1d83
Bruno Parga <brunoparga@gmail.com>
2019-01-17 23:53:13 -0200

    Add to-do's and phases to existing tests
    Two existing tests gain TODO comments that refer to user login and
    date of visit.
    
    Furthermore, the phases of each test have been labeled as per
    https://robots.thoughtbot.com/four-phase-test .

--- spec/features/visitor_chooses_a_bar_spec.rb
+++ spec/features/visitor_chooses_a_bar_spec.rb
@@ -6,17 +6,28 @@ RSpec.feature 'Visitor sees list of bars' do
     let(:bar) { FactoryBot.create(:bar, owner: owner) }
 
     scenario 'sees a bar card' do
+      # setup
       bar
       visit root_path
+
+      # exercise
       click_button('Game on!')
+
+      # verify
       expect(page).to have_selector('.bar-card')
     end
 
     scenario 'clicks a bar card' do
+      # setup
       bar
       visit root_path
+
+      # exercise
       click_button('Game on!')
       page.find('.bar-card-link').click
+
+      # verify
+      # TODO: expect(page).to have_content("Book your #{date} visit at #{bar.name}")
       expect(page).to have_content("Book your visit at #{bar.name}")
     end
   end
--- spec/features/visitor_searches_a_city_spec.rb
+++ spec/features/visitor_searches_a_city_spec.rb
@@ -6,18 +6,28 @@ RSpec.feature 'Visitor comes to the home page' do
     let(:bar) { FactoryBot.create(:bar, owner: owner) }
 
     it 'shows bars available in that city' do
+      # setup
       visit root_path
+
+      # exercise
       fill_in('City', with: bar.city)
       click_button('Game on!')
+
+      # verify
       expect(page).to have_content('available tonight')
     end
   end
 
   context 'searches for a city that does not have bars' do
     it 'shows all the bars available elsewhere' do
+      # setup
       visit root_path
+
+      # exercise
       fill_in('City', with: 'Rome')
       click_button('Game on!')
+
+      # verify
       expect(page).to have_content('Sorry, we still don\'t have any bars in Rome. Here are all our bars:')
     end
   end

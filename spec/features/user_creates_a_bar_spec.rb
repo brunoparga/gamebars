977c94d5cbf93f63454cf216211ccecdcf36ac54
Bruno Parga <brunoparga@gmail.com>
2019-01-18 17:45:48 -0200

    Refactor the bar creation test

--- spec/features/user_creates_a_bar_spec.rb
+++ spec/features/user_creates_a_bar_spec.rb
@@ -1,43 +1,59 @@
 require 'rails_helper'
 
 RSpec.feature 'User wants to create a bar' do
-  scenario 'signs up and creates bar' do
-    # setup
-    visit root_path
+  scenario 'signs up' do
+    click_on_sign_up_link
+    expect(page).to have_content 'Password confirmation'
+  end
 
-    # exercise
+  scenario 'fills out signup form' do
+    fill_out_sign_up_form
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
+
+  scenario 'creates a bar' do
+    fill_out_new_bar_form
+    expect(page).to have_content('This is your bar, Capybara\'s own')
+  end
+
+  scenario 'edits their own bar' do
+    fill_out_edit_bar_form
+    expect(page).to have_content('This is your bar, Capybara\'s very own')
+  end
+
+  private
+
+  def click_on_sign_up_link
+    visit root_path
     Capybara.match = :first
     click_link 'Sign up'
+  end
 
-    # verify
-    expect(page).to have_content 'Sign up'
-
-    ## These should be different scenarios but are put together to preserve
-    ## the session between them.
-
-    # exercise
+  def fill_out_sign_up_form
+    click_on_sign_up_link
     fill_in 'Email', with: 'capybara@test.auto'
     find_field('Password *').fill_in with: 'biggestrodent'
     find_field('Password confirmation *').fill_in with: 'biggestrodent'
     click_button 'Sign up'
+  end
 
-    # verify
-    expect(page).to have_content 'Welcome! You have signed up successfully.'
-
-    ## This is also the same session
-
-    # setup
+  def fill_out_new_bar_form
+    fill_out_sign_up_form
     find('.avatar').click
     click_link('I own a bar')
+    {
+      'Name' => "Capybara's own",
+      'Address' => 'Pantanal matogrossense',
+      'City' => 'Korunbaa',
+      'Opening hours' => 'All the time'
+    }.each { |key, value| fill_in key, with: value }
+    find('#save-bar').click
+  end
 
-    # exercise
-    fill_in 'Name', with: "Capybara's own"
-    fill_in 'Address', with: 'Pantanal matogrossense'
-    fill_in 'City', with: 'Korunbaa'
-    fill_in 'Opening hours', with: 'All the time'
-    find('#create-bar').click
-
-    # verify
-    expect(page).to have_content('Book your visit at Capybara\'s own')
+  def fill_out_edit_bar_form
+    fill_out_new_bar_form
+    click_link('(edit your bar)')
+    fill_in 'Name', with: "Capybara's very own"
+    find('#save-bar').click
   end
 end
